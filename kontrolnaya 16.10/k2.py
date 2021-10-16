99796b3e2889b2886b1c49129eeccf52ce0b87fd
SNick22 <azat3310977@gmail.com>
2021-10-16 09:52:57 +0300

    1

--- kontrolnaya 16.10/k2.py
+++ kontrolnaya 16.10/k2.py
@@ -1,14 +1,15 @@
-A = [11,43,43,14,16,17,17]
-max1 = 0
-max2 = 0
-max3 = 0
+A = [5,4,5,6,9,8,7,7,6,5,4,4,6,7,8,9,0]
+m1 = 0
+m2 = 0
+m3 = 0
 for i in range(len(A)):
-    if A[i] > max1:
-        max1 = A[i]
-for i in range(len(A)):
-    if (A[i] > max2) and (A[i] != max1):
-        max2 = A[i]
-for i in range(len(A)):
-    if (A[i] > max3) and (A[i] != max1) and (A[i] != max2):
-        max3 = A[i]
-print(max3)
+    if A[i]>m1:
+        m3 = m2
+        m2 = m1
+        m1 = A[i]
+    elif A[i]>m2 and A[i] != m1:
+        m3 = m2
+        m2 = A[i]
+    elif A[i]>m3 and A[i] != m1 and A[i] != m2:
+        m3 = A[i]
+print(m3)
